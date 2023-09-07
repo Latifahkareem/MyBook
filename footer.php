9b5d460ff8ce9fcbc28f3b927126e762603e43bd
Latifah Kareem <ilatifah.kareem@gmail.com>
2023-09-07 16:52:08 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,63 +11,63 @@
       </div>
     </div>
     <div class="row">
+      <div class="col-lg-4 ml-auto text-center">
+        <p></p>
+        <p></p>
+      </div>
       <div class="col-lg-4 ml-auto text-center"> <i class="fa fa-user-circle-o fa-3x mb-3 sr-contact"></i>
         <p>Latifah kareem AlQuairi
  - 201203899</p>
-        <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:ilatifah.kareem@gmail.com"> ilatifah.kareem@gmail.com</a></p>
+        <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:ilatifah.kareem@gmail.com">  ilatifah.kareem@gmail.com</a></p>
       </div>
-      <div class="col-lg-4 ml-auto text-center"> <i class="fa fa-user-circle-o fa-3x mb-3 sr-contact"></i>
-        <p>Khansa Ghazi Al-shammari - 201304478</p>
-        <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:Knssssal1122@hotmail.com"> Knssssal1122@hotmail.com</a></p>
-      </div>
-      <div class="col-lg-4 ml-auto text-center"> <i class="fa fa-user-circle-o fa-3x mb-3 sr-contact"></i>
-        <p><h3>Ms. Zahida Malik</h3></p>
-        <p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:z.malik@uoh.edu.sa"> z.malik@uoh.edu.sa</a></p>
+      <div class="col-lg-4 ml-auto text-center">
+        <p><h3></h3></p>
+        <p></p>
       </div>
     </div>
   </div>
 </section>
 
-<section   class="bg-primary"> 
+<section   class="bg-primary">
   <div class="container">
     <div class="row" >
       <?php
 		   $sql_list = "SELECT c.*    FROM  courses c ORDER BY course_id" ;
-		   $r_list = mysql_query($sql_list) ; 
-		   
+		   $r_list = mysql_query($sql_list) ;
+
 		   while($q_list = mysql_fetch_array($r_list))
 	  {
 		  ?>
-       <a  class="btn btn-light btn-xl js-scroll-trigger" href="Courses.php?course_id=<?=$q_list[course_id]?>" > 
+       <a  class="btn btn-light btn-xl js-scroll-trigger" href="Courses.php?course_id=<?=$q_list[course_id]?>" >
         <?=$q_list[course_name]?>
         </a>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      
-      
+
+
       <?php
 	  }
-		  
-		  
+
+
 ?>
 
     </div>
   </div>
-  
+
   </section>
-  
-  
+
+
   </div>
- 
 
-<!-- Bootstrap core JavaScript --> 
-<script src="vendor/jquery/jquery.min.js"></script> 
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
 
-<!-- Plugin JavaScript --> 
-<script src="vendor/jquery-easing/jquery.easing.min.js"></script> 
-<script src="vendor/scrollreveal/scrollreveal.min.js"></script> 
-<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script> 
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<!-- Custom scripts for this template --> 
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+<!-- Custom scripts for this template -->
 <script src="js/creative.min.js"></script>
 </body></html>
